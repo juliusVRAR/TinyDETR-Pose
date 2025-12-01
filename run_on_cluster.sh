dbef1e281ace6c6fb329ff780ac62c949ae60a15
Julius Kühn <julius.kuehn@stud.h-da.de>
2025-12-01 07:09:06 +0000

    train script for cluster

---
+++ run_on_cluster.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+#SBATCH --job-name=train_ycbv
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=256G
+#SBATCH --time=30-00:00:00
+#SBATCH --output=slurm-%x-%j.out
+#SBATCH --error=slurm-%x-%j.err
+#SBATCH --export=DSNAME=bop_datasets,RES=sd,VRARHUB_MESHES=K0472_53030;K0475_53030
+
+DSNAME=lw_detr6d_data
+
+
+DATAPATH=/opt/cache/$USER
+OUTPATH=$DATAPATH/$DSNAME
+
+PATH_TO_BOP=$OUTPATH/bop_datasets
+PATH_TO_WEIGHTS=$OUTPATH/weights/lw-detr
+WORKSPACE=$PWD
+
+gio_mount_nas()
+{
+    mkdir -p ~/dbus
+    XDG_RUNTIME_DIR=~/dbus
+    export DBUS_SESSION_BUS_ADDRESS=`dbus-daemon --fork --print-address --session`
+    gio mount smb://pc3163/nobackup < ~/smbcreds
+}
+
+
+if test ! -d $OUTPATH ; then
+    gio_mount_nas
+    gio copy --progress smb://pc3163/nobackup/cache/jkuehn/$DSNAME.zip $DATAPATH
+    cd $DATAPATH
+    unzip -q $DSNAME.zip 
+fi
+
+IMAGE_NAME=lw-detr6d
+## MAIN
+rootless-docker run --gpus all --shm-size=256g \
+    -v $WORKSPACE:/workspace/LWDETR\
+    -v $PATH_TO_BOP:/workspace/LWDETR/data/datasets/bop \
+    -v $PATH_TO_WEIGHTS:/workspace/LWDETR/data/weights \
+    pc3163.igd.fraunhofer.de:4567/$IMAGE_NAME\
+    bash -c "python /workspace/LWDETR/models/ops/setup.py build install && \
+            python /workspace/LWDETR/main.py \
+                                --lr 1e-4 \
+                                --lr_transformer 2e-5 \
+                                --lr_encoder 1e-5 \
+                                --lr_backbone 1e-6 \
+                                --batch_size 24 \
+                                --weight_decay 1e-4 \
+                                --epochs 1 \
+                                --lr_drop 60 \
+                                --lr_vit_layer_decay 0.8 \
+                                --lr_component_decay 0.7 \
+                                --encoder vit_tiny \
+                                --vit_encoder_num_layers 6 \
+                                --window_block_indexes 0 2 4 \
+                                --out_feature_indexes 1 3 5 \
+                                --dec_layers 3 \
+                                --group_detr 13 \
+                                --two_stage \
+                                --projector_scale P4 \
+                                --hidden_dim 256 \
+                                --sa_nheads 8 \
+                                --ca_nheads 16 \
+                                --dec_n_points 2 \
+                                --bbox_reparam \
+                                --lite_refpoint_refine \
+                                --num_queries 100 \
+                                --ia_bce_loss \
+                                --cls_loss_coef 1 \
+                                --num_select 100 \
+                                --dataset_file ycbv \
+                                --dataset_path /workspace/LWDETR/data/datasets/bop \
+                                --square_resize_div_64 \
+                                --use_ema \
+                                --pretrain_keys_modify_to_load transformer.enc_out_class_embed.0.weight transformer.enc_out_class_embed.1.weight transformer.enc_out_class_embed.2.weight transformer.enc_out_class_embed.3.weight transformer.enc_out_class_embed.4.weight transformer.enc_out_class_embed.5.weight transformer.enc_out_class_embed.6.weight transformer.enc_out_class_embed.7.weight transformer.enc_out_class_embed.8.weight transformer.enc_out_class_embed.9.weight transformer.enc_out_class_embed.10.weight transformer.enc_out_class_embed.11.weight transformer.enc_out_class_embed.12.weight transformer.enc_out_class_embed.0.bias transformer.enc_out_class_embed.1.bias transformer.enc_out_class_embed.2.bias transformer.enc_out_class_embed.3.bias transformer.enc_out_class_embed.4.bias transformer.enc_out_class_embed.5.bias transformer.enc_out_class_embed.6.bias transformer.enc_out_class_embed.7.bias transformer.enc_out_class_embed.8.bias transformer.enc_out_class_embed.9.bias transformer.enc_out_class_embed.10.bias transformer.enc_out_class_embed.11.bias transformer.enc_out_class_embed.12.bias class_embed.weight class_embed.bias \
+                                --grayscale \
+                                --rgb_augmentation \
+                                --tensorboard \
+                                --pretrained_encoder /workspace/LWDETR/data/weights/caev2_tiny_S_300e_objects365.pth \
+                                --pretrain_weights /workspace/LWDETR/data/weights/LWDETR_tiny_30e_objects365.pth \
+                                --matcher_type "6d""
+                                    
+        
+
+## OUTPUT
+# zip & copy the data back
+cd $OUTPATH/../
+zip -q -r $DSNAME.zip $DSNAME
+if test ! -z "$SLURM_JOB_ID"; then
+    XDG_RUNTIME_DIR=~/dbus
+    export DBUS_SESSION_BUS_ADDRESS=`dbus-daemon --fork --print-address --session`
+    gio mount smb://pc3163/nobackup < ~/smbcreds
+    gio copy --progress $DSNAME.zip smb://pc3163/nobackup/cache/$USER
+fi
