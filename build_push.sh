e94bc4c0913599799750cc4a934dfeda6c24432a
Julius <julius.kuehn@igd.fraunhofer.de>
2026-08-31 15:42:47 +0200

    Update files and readme

--- build_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-IMAGE_NAME=lwdetr6d-duc
-docker build -f Dockerfile.6d -t pc3163.igd.fraunhofer.de:4567/$IMAGE_NAME .
-docker push pc3163.igd.fraunhofer.de:4567/$IMAGE_NAME  
-
-
--- build_push_trt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-IMAGE_NAME=lw-detr6d_trt
-docker login pc3163.igd.fraunhofer.de:4567
-docker build -f Dockerfile.trt -t pc3163.igd.fraunhofer.de:4567/$IMAGE_NAME .
-#docker push pc3163.igd.fraunhofer.de:4567/$IMAGE_NAME  
-
-
